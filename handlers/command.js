3377a2d738f0fdd34c688dad436601f6676dc441
aKaan47_ <74306156+Azulix@users.noreply.github.com>
2023-08-11 12:17:25 +0200

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -2,7 +2,7 @@ const { readdirSync } = require("fs");
 
 const ascii = require("ascii-table");
 let table = new ascii("Commands");
-table.setHeading("Command", "Load status");
+table.setHeading("Commande", "Status");
 
 module.exports = (client) => {
     readdirSync("./commands/").forEach(dir => {
